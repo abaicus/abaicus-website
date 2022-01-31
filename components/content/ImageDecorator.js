c544561bc43c5a47714e715d9d7bc81c5cc1b69b
abaicus <baicusandrei@me.com>
2022-02-01 01:34:52 +0200

    fix: image decorators

--- components/content/ImageDecorator.js
+++ components/content/ImageDecorator.js
@@ -2,11 +2,11 @@ export default function ImageDecorator({ overlay = false }) {
     if (!overlay) {
         return (
             <>
-                <div className="absolute pointer-events-none w-[250px] h-[250px] -right-5 -bottom-5 mix-blend-multiply bg-sky-400 rounded-lg shadow-lg shadow-sky-200 transition-all 
+                <div className="absolute pointer-events-none w-[30%] h-[55%] -right-5 -bottom-5 mix-blend-multiply bg-sky-400 rounded-lg shadow-lg shadow-sky-200 transition-all 
     group-hover:bg-orange-400 group-hover:shadow-orange-200 group-hover:-right-10 group-hover:-bottom-10 group-hover:scale-110
     dark:mix-blend-hard-light dark:shadow-none dark:opacity-75"/>
 
-                <div className="absolute pointer-events-none w-[150px] h-[150px] -left-5 -top-5 mix-blend-multiply bg-red-500 rounded-lg shadow-lg shadow-red-200 transition-all
+                <div className="absolute pointer-events-none w-[20%] h-[37%] -left-5 -top-5 mix-blend-multiply bg-red-500 rounded-lg shadow-lg shadow-red-200 transition-all
     group-hover:bg-teal-400 group-hover:shadow-teal-200 group-hover:-left-10 group-hover:-top-10 group-hover:scale-110 
     dark:mix-blend-hard-light dark:shadow-none dark:opacity-75"/>
             </>
